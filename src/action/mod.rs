ee8056a40354b0a23b6ae702885f4cb6e84151b9
Rohan <lolcactusman2@gmail.com>
2022-08-27 15:00:26 +0100

    Added doc comments

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -6,6 +6,23 @@ use io::Write;
 use std::fs;
 use std::io;
 
+/// A macro that takes in a variadic amount of arguments
+/// and creates files for each argument. This macro allows
+/// you to add content inside of the file by adding =>,
+/// forwarded by a string.
+/// 
+/// # Example
+/// ```
+/// fn create_files() -> std::io::Result<()> {
+///    use crate::files_create;
+/// 
+///    files_create!(
+///     // # NAME     # CONTENT
+///     "test.txt" => "Hello, World",
+///     "inner.html" => ""
+///    ); 
+/// }
+/// ```
 #[macro_export]
 macro_rules! files_create {
     () => {};
@@ -19,6 +36,23 @@ macro_rules! files_create {
     };
 }
 
+/// This macro, similar to [`files_create`], allows
+/// you to create any amount of directories. It is
+/// recursive which means it can create parent folders
+/// if they do not exist.
+/// 
+/// # Example
+/// ```
+/// fn create_directories() -> std::io::Result<()> {
+///     use crate::dir_create;
+/// 
+///     dir_create!(
+///         "directory",
+///         "top/subdirectory/",
+///         "upper/sub/bottom/inner"
+///     );
+/// }
+/// ```
 #[macro_export]
 macro_rules! dir_create {
     () => {};
