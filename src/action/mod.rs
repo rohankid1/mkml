7ed2c1cd33fd4cce720ab6888c3262ef0794cc5c
Rohan <lolcactusman2@gmail.com>
2022-08-27 14:04:18 +0100

    Added helpful macros to reduce code repetition

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -3,15 +3,38 @@ mod init;
 pub use init::initialize_project;
 
 use io::Write;
-use std::io;
 use std::fs;
+use std::io;
+
+#[macro_export]
+macro_rules! files_create {
+    () => {};
+
+    ($($fname:expr $(=> $content:expr)?),*) => {
+        $(
+            {
+                create_file($fname, $($content)*)?
+            }
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! dir_create {
+    () => {};
+
+    ($($dir_name:expr),*) => {
+        $(
+            {
+                std::fs::DirBuilder::new()
+                    .recursive(true)
+                    .create($dir_name)?
+            }
+        )*
+    };
+}
 
 pub fn create_file(name: &str, contents: &str) -> io::Result<()> {
-    // if fs::metadata(name)?.is_file() { // then the file must already exists
-    //     log::warn!("File already exists: `{name}`");
-    //     return Ok(());
-    // }
-
     let mut file = fs::File::create(name)?;
 
     write!(file, "{contents}")?;
