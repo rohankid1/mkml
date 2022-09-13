3d98283dc3199a7c4b59e9c695f6e36f12f5f06b
Rohan <lolcactusman2@gmail.com>
2022-09-13 17:49:34 +0100

    Added stuff

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,36 +1,50 @@
+mod clone;
 mod init;
 
+pub use clone::clone_project;
 pub use init::initialize_project;
 
 use io::Write;
-use std::fs;
-use std::io;
+use owo_colors::OwoColorize;
+use std::{fs, io};
 
 /// A macro that takes in a variadic amount of arguments
 /// and creates files for each argument. This macro allows
 /// you to add content inside of the file by adding =>,
 /// forwarded by a string.
-/// 
+///
 /// # Example
 /// ```
-/// fn create_files() -> std::io::Result<()> {
+/// fn create_two_files() -> std::io::Result<()> {
 ///    use crate::files_create;
-/// 
-///    files_create!(
+///
+///    create_files!(
 ///     // # NAME     # CONTENT
 ///     "test.txt" => "Hello, World",
 ///     "inner.html" => ""
-///    ); 
+///    )?;
 /// }
 /// ```
 #[macro_export]
-macro_rules! files_create {
+macro_rules! create_files {
     () => {};
 
     ($($fname:expr $(=> $content:expr)?),*) => {
         $(
+                create_file($fname, $($content)*)?;
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! files {
+    ($({ $arg:tt => $cnt:expr $(; $fmt:expr),* }),*) => {
+        $(
             {
-                create_file($fname, $($content)*)?
+                let mut file = std::fs::File::create(format!($arg $(, $fmt)*))?;
+
+                core::write!(file, $cnt)?;
+                file.flush()?;
             }
         )*
     };
@@ -40,29 +54,30 @@ macro_rules! files_create {
 /// you to create any amount of directories. It is
 /// recursive which means it can create parent folders
 /// if they do not exist.
-/// 
+///
 /// # Example
 /// ```
 /// fn create_directories() -> std::io::Result<()> {
 ///     use crate::dir_create;
-/// 
-///     dir_create!(
+///
+///     let formatted_text = "upper/inner/inner/inner/end";
+///
+///     create_dirs!(
 ///         "directory",
 ///         "top/subdirectory/",
-///         "upper/sub/bottom/inner"
-///     );
+///         "upper/sub/bottom/inner",
+///        "{formatted_text}"
+///     )?;
 /// }
 /// ```
 #[macro_export]
-macro_rules! dir_create {
+macro_rules! create_dirs {
     () => {};
 
-    ($($dir_name:expr),*) => {
+   ($($arg:tt),*) => {
         $(
             {
-                std::fs::DirBuilder::new()
-                    .recursive(true)
-                    .create($dir_name)?
+                std::fs::create_dir_all(&format!($arg))?
             }
         )*
     };
@@ -73,4 +88,12 @@ pub fn create_file(name: &str, contents: &str) -> io::Result<()> {
 
     write!(file, "{contents}")?;
     file.flush()
+}
+
+pub fn success(msg: &str) {
+    println!("{}: {msg}", "Success".green());
+}
+
+pub fn fail(msg: &str) {
+    eprintln!("{}: {msg}", "Error".red());
 }
