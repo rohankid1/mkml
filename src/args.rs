03ca44d75246d7f779e941bb7cca4733cc84eb35
Rohan <lolcactusman2@gmail.com>
2022-09-13 17:45:19 +0100

    Added debug build notice

--- src/args.rs
+++ src/args.rs
@@ -11,30 +11,52 @@ pub struct App {
     /// 4: Error
     #[clap(short, long, action = clap::ArgAction::Count)]
     pub log: u8,
-    /// What to do - e.g. initialize
+    /// Action to run - i.e. initialize or clone
     #[clap(subcommand)]
     pub action: Action,
+
+    #[cfg(debug_assertions)]
+    #[clap(skip)]
+    pub debug: bool,
 }
 
 #[derive(Debug, Subcommand, Clone)]
 pub enum Action {
     /// Create a HTML project
     Init(InitProject),
+    /// Clone an existing project
+    Clone(CloneProject),
 }
 
 #[derive(Debug, Args, Clone)]
 pub struct InitProject {
     /// The name of the project
-    #[clap(short, long)]
     pub name: Option<String>,
 
     /// Don't include a certain language to be added
     #[clap(short, long, arg_enum)]
     pub exclude: Option<Exclude>,
 
-    /// Creates a basic HTML template file in a directory (if the --name or -n flag is provided)
+    /// Creates a basic HTML template file in a directory, if the --name or -n flag is provided
     #[clap(short, long)]
     pub minimal: bool,
+}
+
+#[derive(Debug, Args, Clone)]
+pub struct CloneProject {
+    /// The path to the project to clone.
+    pub path: String,
+    /// Rename the cloned project.
+    /// This is required because
+    /// cloned directories cannot
+    /// have the exact name.
+    pub rename: String,
+    /// Allow files that exist to be overwritten
+    #[clap(short, long)]
+    pub overwrite: bool,
+    /// Skip files that already exist
+    #[clap(short, long)]
+    pub skip_exist: bool,
 }
 
 #[derive(Debug, Clone, ArgEnum, Default)]
