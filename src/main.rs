a43145687beb0499f9cd87b19d1b7edc134d618e
Rohan <lolcactusman2@gmail.com>
2022-09-13 17:48:42 +0100

    Added debug build notice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use clap::Parser;
 
 mod action;
@@ -9,11 +11,35 @@ fn main() {
     // initialize the logger
     pretty_env_logger::init();
 
-    // parse the command line flags and others
-    let args = args::App::parse();
+    let mut args = std::env::args().collect::<Vec<_>>();
 
-    // TODO: get it to actually log
-    // when on a certain level.
+    profile_time::debug_time! {
+
+                if !args.contains(&"-D".to_owned()) {
+
+                println!("If you are using this tool normally, you should be running the release mode;
+        the debug mode (which you are running currently) is meant for developers to debug their application. 
+        See https://github.com/rohankid1/mkml/ on how to get the release mode which is more optimised.");
+
+                println!("If you are debugging or a developer and want to remove this message, run the application again
+                but with -D");
+
+            }
+
+        let search = args.binary_search(&"-D".to_owned());
+
+        if search.is_ok() {
+            args.remove(search.unwrap());
+        }
+    }
+
+    // parse the command line flags and others
+    let args = args::App::parse_from(args);
+
+    // TODO: make it able to change the log level at
+    // runtime. Example: the flag -ll changes the log
+    // level to info at runtime, before anything else
+    // runs.
     // match args.log {
     //     1 => env::set_var("RUST_LOG", "debug"),
     //     2 => env::set_var("RUST_LOG", "info"),
@@ -29,6 +55,6 @@ fn main() {
     // releases
     match &args.action {
         Action::Init(_) => action::initialize_project(&args),
+        Action::Clone(_) => action::clone_project(&args),
     }
-    
 }
